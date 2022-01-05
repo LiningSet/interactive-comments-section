133decefacf30b9ed70441de1cda8b74ed9d1422
liningSet <sajedbitcoin96@gmail.com>
2022-01-05 17:11:04 +0330

    modified js

--- app/js/script.js
+++ app/js/script.js
@@ -9,10 +9,8 @@ let comments;
 let replies;
 let currentComment;
 
-fetch("/interactive-comments-section-main/app/js/data.json")
-  .then((e) => {
-    return e.json();
-  })
+fetch("./data.json")
+  .then((response) => response.json())
   .then((data) => {
     currentUser = data.currentUser;
     comments = data.comments;
